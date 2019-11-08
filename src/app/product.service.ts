39341a77a9f453ac39adb3a27febc22f2940eefa
queenfiona <fionaitya@gmail.com>
2019-11-08 21:20:35 +0300

    Add an albumUrl property in ProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,7 @@ import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
+  private _albumUrl: string = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
